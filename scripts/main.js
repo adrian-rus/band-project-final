fff1c4c349b46c11ec9371f09e483a3ca8fa16b7
Adi <adrian.rus@live.com>
2016-02-21 16:56:06 +0000

    css adjustments for responsivness

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,8 @@ $(document).ready(function(){
       $('#play1').toggleClass('show');
     });
 
-    $('#show-play2').click(function(){
-      $('#play2').toggleClass('show');
-    });
-
-    $('#show-play3').click(function(){
-      $('#play3').toggleClass('show');
-    });
-
-    $('#show-play4').click(function(){
-      $('#play4').toggleClass('show');
+    $('#img1').click(function(){
+      $('#play1').toggleClass('show');
     });
 
     $('.name').click(function(){
